8897be0d5f541f559aae61cc67a69beaac1f1acf
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-19 22:57:40 -0600

    created starting page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<meta charset="UTF-8">
+	<title>Tramway Venture Partners</title>
+</html>
+<body>
+	<main>
+		<p>Website in progress, please be patience great things are coming</p>
+	</main>
+</body>
