e6a721458b5ba9927d547b742a4c6556ea454025
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-26 22:44:17 -0600

    added list and home page image to plan

--- plan/index.php
+++ plan/index.php
@@ -6,7 +6,19 @@
 	</head>
 	<body>
 		<main>
-
+<h1>Tramway Venture Partners Website</h1>
+			<p>The site will be organized into the following pages</p>
+			<ul>
+				<li>Home page</li>
+				<li>About page</li>
+				<li>Team page</li>
+				<li>Portfolio</li>
+				<li>News</li>
+				<li>Contact</li>
+			</ul>
+<h2>Notes:</h2>
+			<p></p>
+			<img class="img-fluid" src="../public_html/images/homepage.png">
 		</main>
 	</body>
 </html>
