5e8613eb5c2a18c01de4d04be01b8d1d59d7d0f0
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-27 22:24:12 -0600

    added the rest of the wireframe images all pages for the desktop and mobile pages

--- plan/index.php
+++ plan/index.php
@@ -17,8 +17,13 @@
 				<li>Contact</li>
 			</ul>
 <h2>Notes:</h2>
-			<p></p>
+			<p><Strong>All Pages</Strong> will have the Tramway Venture Partner's logo and the nav bar at the top.</p>
 			<img class="img-fluid" src="../public_html/images/homepage.png">
+			<h3>Home Page</h3>
+			<p><strong>The Home Page</strong> Will have a picture of the Sandia Peak Tramway and on the picture will be a quote or slogan. Below will be a short and sweet paragraph about Tramway Venture Partners similar to an elevator speech</p>
+			<img class="img-fluid" src="../public_html/images/about.png">
+			<img class="img-fluid" src="../public_html/images/team.png">
+			<img class="img-fluid" src="../public_html/images/"
 		</main>
 	</body>
 </html>
