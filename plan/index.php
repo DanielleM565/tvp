cca120c5719166debc2653bf87571a5098d1e621
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-28 00:32:08 -0600

    added some bootstrap to the plan index.php

--- plan/index.php
+++ plan/index.php
@@ -2,8 +2,23 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<link rel="stylesheet" href="style.css" type="text/css">
 		<title>TVP</title>
+
+		<!-- bootstrap -->
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
 	</head>
 	<body>
 		<main>
@@ -17,7 +32,6 @@
 				<li>News</li>
 				<li>Contact</li>
 			</ul>
-<h2>Notes</h2>
 			<p><Strong>All Pages</Strong> will have the Tramway Venture Partner's logo and the nav bar at the top.</p>
 			<img class="img-fluid" src="../public_html/images/homepage.png">
 			<h3>Home Page</h3>
