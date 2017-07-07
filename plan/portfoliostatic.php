bb7b573ef0abb56ef55a50067dbe844fc6862dae
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-07-06 18:08:55 -0600

    changed bootstrap on portfolio

--- plan/portfoliostatic.php
+++ plan/portfoliostatic.php
@@ -71,8 +71,12 @@
 						<div class="page-header">
 							<h2>Portfolio 1 <small> blah blah blah</small></h2>
 						</div>
-						<i class="fa fa-users fa-5x" aria-hidden="true"></i>
-						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+						<div class="col-md-3">
+							<i class="fa fa-users fa-5x" aria-hidden="true" id="icon"></i>
+						</div>
+						<div class="col-md-9">
+							<p> <br>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+						</div>
 					</div>
 				</div>
 			</main>
