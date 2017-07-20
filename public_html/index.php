d07dff2a39fe486ccd672944eaeb1d1d1402d65d
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-07-20 14:47:32 -0600

    added some SEO meta tags and the rest of the pages news, portfolio, team etc...

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@
 	<title>Tramway Venture Partners</title>
 </html>
 <body>
+	<!-- in case of crash -->
 	<main>
 		<p>Website in progress, please be patience great things are coming</p>
 	</main>
---
+++ public_html/portfolio.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tramway Venture Partners</title>
+
+		<meta name="keywords" content="venture capitol, Venture Partners, catalyst fund, start-up, invest, bio-tech, life science, DNA sequencing, NM, New Mexico, Abq, abq, Albuquerque, southwest region, Waneta Tuttle, John Rockwell, Stu Rose, Brad Vale"
+
+		<meta name="description" content="Tramway Venture Partners is a highly experienced team with Wantea Tuttle, John Rockwell, Stu Rose, and Brad Vale. Together we are a seed and early stage venture fund in life sciences and health care start-ups in New Mexico. Based in Albuquerque and funded by the Catalyst fund, we will seek opportunities to invest in companies with significant intellectual property positions, clear go to market strategies, large addressable markets that are expected to require less than a total of $30M to exit">
+
+		<meta name="generator" content="Isles Designs">
+
+		<meta name="copyright" content="Tramway Venture Partners">
+
+		<!-- mobile first -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- if IE 9 -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!--font awesome for icons and stuff-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!-- google fonts-->
+		<link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre|Old+Standard+TT|Source+Serif+Pro" rel="stylesheet">
+
+		<!-- custom CSS -->
+		<link rel="stylesheet" href="./css/style.css" type="text/css">
+
+	</head>
+	<body>
+		<header>
+			<!--Logo goes here -->
+
+			<!--Nav par or nav pills go here -->
+			<div class="navbar navbar-default navbar-static-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<!-- for mobile view navbar icon-->
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle Navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- navbar -->
+						<a class="navbar-brand" href="#">Tramway Venture Partners</a>
+					</div>
+					<div class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="home.php">Home</a></li>
+							<li><a href="about.php">About</a></li>
+							<li><a href="team.php">Team</a></li>
+							<li class="active"><a href="portfolio.php">Portfolio</a></li>
+							<li><a href="news.php">News</a></li>
+							<li><a href="contact.php">Contact</a></li>
+						</ul>
+					</div> <!-- navbar collapse -->
+				</div> <!-- container -->
+			</div>
+		</header>
+		<main>
+
+			<div class="container">
+				<div class="row">
+					<h1>Portfolio</h1>
+					<div class="page-header">
+						<h2>Portfolio 1 <small> blah blah blah</small></h2>
+					</div>
+					<div class="col-md-3">
+						<i class="fa fa-users fa-5x" aria-hidden="true" id="icon"></i>
+					</div>
+					<div class="col-md-9">
+						<p> <br>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+					</div>
+				</div>
+			</div>
+
+
+
+			<div class="container">
+				<div class="row">
+					<div class="page-header">
+						<h2>Portfolio 2 <small> blah blah blah</small></h2>
+					</div>
+					<div class="col-md-3">
+						<i class="fa fa-users fa-5x" aria-hidden="true" id="icon"></i>
+					</div>
+					<div class="col-md-9">
+						<p> <br>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+					</div>
+				</div>
+			</div>
+
+
+
+			<!-- I need my space -->
+			<div class="container">
+				<div class="row">
+					<br><br>
+				</div>
+			</div>
+
+
+
+		</main>
+
+	</body>
+
+
+	<footer>
+		<br>
+		<p> &copy; 2017 All rights belong to Tramway Venture Partners 	</p>
+		<br>
+	</footer>
+
+</html>
